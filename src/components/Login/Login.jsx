9a105d13a97b2a7a2a3f82550e3a01f2f56d7f9a
abdennour001 <amokranabdennour@gmail.com>
2020-08-22 20:51:16 +0100

    setting up the login page 🔑

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Login.css";
+import loginUrl from "./utils/spotify";
+
+function Login() {
+    return (
+        <div className="login">
+            <img
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt=""
+            />
+            <a className="login__button" href={loginUrl}>
+                LOGIN TO SPOTIFY
+            </a>
+        </div>
+    );
+}
+
+export default Login;
