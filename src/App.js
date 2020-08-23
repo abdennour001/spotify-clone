303de83803978c12ab6c4d442c0210a669daca91
abdennour001 <amokranabdennour@gmail.com>
2020-08-23 12:14:37 +0100

    fix login logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import UrlSearchParams from "./utils/url";
 
 function App() {
+    useEffect(() => {
+        var params = new UrlSearchParams(window.location.href);
+        const _token = params.hash ? params.hash.get("access_token") : null;
+        window.location.hash = "";
+        if (_token) {
+            console.log("Connected");
+        } else {
+            console.log("Not Connected Yet");
+        }
+    }, []);
+
     return (
         <div className="App">
             <Login />
---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Player.css";
+
+function Player() {
+    return (
+        <div className="player">
+            <h1>Hello to your player</h1>
+        </div>
+    );
+}
+
+export default Player;
